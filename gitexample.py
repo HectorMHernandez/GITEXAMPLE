62df1877c80f141da22b4089d071afba6c05b18b
Hector Hernandez <marioirenehernandez@gmail.com>
2024-05-03 11:00:54 -0400

    new features here

--- gitexample.py
+++ gitexample.py
@@ -5,4 +5,14 @@ class TwoSum(object):
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    return [i, j]
+                    return [i, j]              
+print ("I would like to add a new line here")
+               
+#RAAAAAAAAHHHHHHH
+# haslkdjfhkaljsdfkajhdflads
+#do we understand git now?
+# I really hope so
+# I am pretty sure that I am getting it, New changes are marked green in the 
+# buffer zone, and then you can add them to the staging area, and then commit
+# and it seems that there is an indexed version of the code with essentially acts
+# as a checkpoint, and then you can push the code to the master branch
