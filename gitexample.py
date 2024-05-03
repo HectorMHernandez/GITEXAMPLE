b78053409ccf58c212a46e5c505cdee49b53d9e6
Hector Hernandez <marioirenehernandez@gmail.com>
2024-05-03 10:26:09 -0400

    First Commit

---
+++ gitexample.py
@@ -0,0 +1,8 @@
+#this is an example of python
+
+class TwoSum(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
